be497646d8d60a8e11c2880103a1d43859389fdc
grothlin-iut90 <gael.rothlin@edu.univ-fcomte.fr>
2025-12-29 16:24:38 +0100

    new: button to stop editing

--- resources/views/collections/show.blade.php
+++ resources/views/collections/show.blade.php
@@ -18,6 +18,7 @@
                     <label for="">Title</label><input type="text" name="name" value="{{ $collection->name }}" class="border-b border-gray-300 focus:outline-none focus:border-blue-500" placeholder="Collection Name" required>
                     <label for="">Description</label><input type="text" name="description" value="{{ $collection->description }}" class="border-b border-gray-300 focus:outline-none focus:border-blue-500" placeholder="Collection Description">
                 </div>
+                <button type="button" onclick="window.location.href='{{ route('collections.show', ['collection' => $collection, 'edit' => false]) }}'" class="button-secondary ml-2">Stop Editing</button>
                 <button type="submit" class="button-primary ml-2">Finish Edit</button>
             </form>
             @endif 
@@ -30,9 +31,17 @@
         </a>
     </x-slot>
     <div class="container">
-        @if (!$editing)
-        <h3>{{ $collection->description }}</h3>
-        @endif
+        <div class="description-first">
+            @if (!$editing)
+            <h3>{{ $collection->description }}</h3>
+            <a href="{{ route('collections.show', ['collection' => $collection, 'edit' => true]) }}" class="button-edit" title="Edit Collection">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 .196-.12l6.813-6.814z"/>
+                    <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+                </svg>
+            </a>
+            @endif
+        </div>
         <hr>
         <table class="min-w-full" style="background-color: var(--bg-card); margin-top: 20px; border-radius: 8px; overflow: hidden;">
             <thead>
@@ -76,7 +85,7 @@
         margin: 0 auto;
     }
 
-    .header-first {
+    .header-first, .description-first {
         display: flex;
         align-items: center;
         gap: 15px;
