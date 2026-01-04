d100cfcb8be10d16dd0b2cad3e39ab93b97be2b0
grothlin-iut90 <gael.rothlin@edu.univ-fcomte.fr>
2026-01-04 14:18:02 +0100

    small visual fix button

--- resources/views/collections/create.blade.php
+++ resources/views/collections/create.blade.php
@@ -4,8 +4,8 @@
             <h2 class="font-semibold text-xl leading-tight" style="color: var(--text-header);">
                 {{ __('Create New Collection') }}
             </h2>
-            <a href="{{ route('collections.index') }}" class="button-return">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+            <a href="{{ route('collections.index') }}" class="button-return" style="margin-left: 20px;">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" >
                     <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
                 </svg>
                 Back
